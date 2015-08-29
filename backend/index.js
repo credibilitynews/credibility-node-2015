608dd414e03dcbaabb4007022b62f59c1ae3cdb5
alvinsj <email.to.alvin@gmail.com>
2015-08-29 17:22:55 +0700

    added backend folder, prepare for falcor

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+var jsx = require('node-jsx').install();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var FalcorServer = require('falcor-express');
+//var RouterFactory = require('./src/js/router-factory');
+var React = require('react/addons');
+var APP = require('../frontend/src/js/app.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('.', express.static('public'));
+
+//app.use('/model.json', FalcorServer.dataSourceRoute(function(req, res){
+//    return RouterFactory("1");
+//}));
+
+//app.use('/public', express.static('./public'));
+app.get('/public/*', function(req, res){
+    console.log(req.url)
+    res.sendFile(req.url, {root: "./backend"});
+});
+app.get('/*', function(req, res){
+    var reactHtml = React.renderToString(APP({url: req.url}));
+    res.render('index.ejs', {reactOutput: reactHtml});
+});
+
+
+app.set('views', __dirname + '/views');
+
+
+var server = app.listen(8000, function(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log("navigate to http://localhost:8000");
+});
