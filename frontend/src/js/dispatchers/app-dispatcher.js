e6edf0636286a69a728cd6044c91bd082290709a
alvinsj <email.to.alvin@gmail.com>
2015-09-07 02:17:59 +0700

    upgrade dispatcher

--- frontend/src/js/dispatchers/app-dispatcher.js
+++ frontend/src/js/dispatchers/app-dispatcher.js
@@ -1,21 +1,21 @@
-var Dispatcher = require("./dispatcher"),
-    merge = require("object.assign");
+var Dispatcher = require("flux").Dispatcher,
+    assign = require("object-assign");
 
 var PayloadSources = require('../constants/app-constants').PayloadSources;
 
-var AppDispatcher = merge(Dispatcher.prototype, {
-    handleViewAction: function(action){
-        this.dispatch({
-            source: PayloadSources.VIEW_ACTION,
-            action: action
-        })
-    },
-    handleServerAction: function(action){
-        this.dispatch({
-            source: PayloadSources.SERVER_ACTION,
-            action: action
-        });
-    }
+Dispatcher.prototype = assign(Dispatcher.prototype, {
+  handleViewAction: function(action){
+    this.dispatch({
+      source: PayloadSources.VIEW_ACTION,
+      action: action
+    })
+  },
+  handleServerAction: function(action){
+    this.dispatch({
+      source: PayloadSources.SERVER_ACTION,
+      action: action
+    });
+  }
 });
 
-module.exports = AppDispatcher;
+module.exports = new Dispatcher();
