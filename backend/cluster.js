dea4ddc968818ec6e440afafca4ac060c403ad8e
alvinsj <email.to.alvin@gmail.com>
2015-09-22 02:13:08 +0700

    default to 1 worker

--- backend/cluster.js
+++ backend/cluster.js
@@ -3,7 +3,7 @@ var cluster = require('express-cluster');
 cluster(function(worker){
     return require('./index');
 }, {
-    count: process.env.WEB_CONCURRENCY || 2,
+    count: process.env.WEB_CONCURRENCY || 1,
     verbose: true,
     respawn: true
 });
