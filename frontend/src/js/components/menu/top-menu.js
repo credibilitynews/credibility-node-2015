07c38c4c057c70331e1a138490e28b523204e7cb
alvinsj <email.to.alvin@gmail.com>
2015-11-27 23:13:14 +0700

    minor style fix for top menu

--- frontend/src/js/components/menu/top-menu.js
+++ frontend/src/js/components/menu/top-menu.js
@@ -32,7 +32,7 @@ class TopMenu extends React.Component {
         return (
             <div className="top-menu">
                 <ul className="nav nav-pills">
-                    <li className="nav-item">Tags: </li>
+                    <li className="nav-item">Most Tags: </li>
                     {this.renderItem(this.state.items)}
                 </ul>
             </div>);
